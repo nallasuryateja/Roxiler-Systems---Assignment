6d9e256ca9cdc438fddd63e9b928344aa5c46c5c
nallasuryateja <nallasuryatheja@gmail.com>
2023-07-22 17:37:25 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "transaction.db");
+
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/", async (request, response) => {
+  const getMoviesQuery = `
+    SELECT
+      *
+    FROM
+      mytable;`;
+  const moviesArray = await database.all(getMoviesQuery);
+  response.send(moviesArray);
+});
+
+module.exports = app;
